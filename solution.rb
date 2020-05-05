ad664b20bf34a18dd1d96eb3c0749fe8d27efa2e
github username <github email address>
2020-05-05 09:39:36 -0400

    rank & suit nested loop

--- solution.rb
+++ solution.rb
@@ -1,7 +1,23 @@
 class Deck
 
+    @@cards = []
+
+    def initialize(cards)
+        @@cards << 
+    end
+
 end
 
 class Card
+
+    i = 0
+    while i < rank.length
+        i += 1
+        j = 0
+        while j < suit.length
+            pair = rank[i] + suit[j]
+            j += 1
+        end
+    end
 
 end
